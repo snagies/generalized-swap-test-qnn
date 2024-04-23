540ae05c4bf25fba74a0e3543942fd261eebe2f8
Sebastian Nagies <sebastian.nagies@unitn.it>
2024-04-24 00:23:02 +0200

    forward pass

--- algorithm/module.py
+++ algorithm/module.py
@@ -1,17 +1,20 @@
 import numpy as np
-from qiskit import QuantumCircuit, QuantumRegister
-from qiskit_machine_learning.circuit.library import RawFeatureVector, QNNCircuit
-from qiskit_machine_learning.neural_networks import SamplerQNN
-from qiskit.circuit import Parameter, ParameterVector
 
+from qiskit_machine_learning.circuit.library import RawFeatureVector, QNNCircuit
+from qiskit.providers.basic_provider import BasicProvider
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
+#from qiskit_machine_learning.neural_networks import SamplerQNN
+#from qiskit.circuit import Parameter, ParameterVector
+from helpers import normalize
 
 def swap_test(dim, fm_explicit = False):
     
+    cr = ClassicalRegister(1, 'outcome')
     control = QuantumRegister(1, 'control')
     x = QuantumRegister(dim, 'x')
     weights = QuantumRegister(dim, 'weights')
     
-    qc = QuantumCircuit(control, x, weights)
+    qc = QuantumCircuit(cr, control, x, weights)
     
     #ansatz
     qc.h(control)
@@ -21,9 +24,11 @@ def swap_test(dim, fm_explicit = False):
     
     qc.h(control)
     
+    qc.measure(0, 0)
+    
     if fm_explicit:
         
-        fm = QuantumCircuit(control, x, weights)
+        fm = QuantumCircuit(cr, control, x, weights)
         
         #feature map
         fm.compose(RawFeatureVector(2**dim), x, inplace = True)
@@ -35,14 +40,26 @@ def swap_test(dim, fm_explicit = False):
     return qc
 
 
-#test = normalize(np.random.rand(4))
+test = normalize(np.random.rand(4))
+test2 = normalize(np.random.rand(4))
+test2 = test2 - test*np.dot(test,test2)
 
-#fm, ae = swap_test(2, fm_explicit = True)
-#fm.assign_parameters(test, inplace = True)
-#ae.assign_parameters(test, inplace = True)
+fm, ae = swap_test(2, fm_explicit = True)
+fm.assign_parameters(test, inplace = True)
+ae.assign_parameters(test2, inplace = True)
 
-#qc = QNNCircuit(feature_map = fm, ansatz = ae)
+qc = QNNCircuit(feature_map = fm, ansatz = ae)
 
-#qnn = SamplerQNN(circuit=qc)
 
-#first transpile then give to sampler qnn?
+
+provider = BasicProvider()
+backend = provider.get_backend("basic_simulator")
+
+
+
+'''
+trans_qc = transpile(qc, backend)
+job = backend.run(trans_qc, shots = 1024)
+results  = job.result()
+counts = results.get_counts()
+'''
--- algorithm/qnn.py
+++ algorithm/qnn.py
@@ -1,2 +1,65 @@
 import numpy as np
+from qiskit_machine_learning.circuit.library import QNNCircuit
+from qiskit import transpile
+from qiskit.providers.basic_provider import BasicProvider
+from module import swap_test
+from helpers import normalize
 
+
+class ScalableQNN():
+    
+    def __init__(self, mod_size, mod_num = 1):
+        
+        self.mod_size = mod_size
+        self.mod_num = mod_num
+        
+        self.dim = 2**self.mod_size 
+        
+        #TODO different weights initializations
+        self.weights = normalize(np.random.normal(0, 1, self.dim * self.mod_num))
+        self.coefficients = normalize(np.ones(self.mod_num))
+        
+        self.loss = None
+        self.output = None
+        
+        self.fm, self.ae = swap_test(self.mod_size, fm_explicit = True)
+        
+    def update_weights(self, data, learning_rate = 0.1):
+        gradient = 0
+        self.weights = normalize(self.weights - learning_rate * gradient)
+    
+    def squared_loss(self, data):
+        loss = 0
+        self.loss = loss
+        
+    def forward_pass(self, x, shots = 100):
+        #calculate qnn output for one sample (fitting completely onto modules)
+        
+        assert (self.dim * self.mod_num) % len(x) == 0
+        
+        p0 = np.zeros(self.mod_num)
+        
+        for i in range(self.mod_num):
+            
+            self.fm, self.ae = swap_test(self.mod_size, fm_explicit = True)
+            
+            self.fm.assign_parameters(normalize(x[i*self.dim:(i+1)*self.dim]), inplace = True) 
+            self.ae.assign_parameters(self.weights[i*self.dim:(i+1)*self.dim], inplace = True)
+            
+            self.qnn = QNNCircuit(feature_map = self.fm, ansatz = self.ae)
+            
+            #TODO sample module to get P(0)
+            #...
+            p0[i] = 0.5
+            
+        sample_output = np.dot(self.coefficients, p0)
+        
+        return sample_output, p0
+        
+        
+        
+        #provider = BasicProvider()
+        #backend = provider.get_backend("basic_simulator")
+        
+        #trans_qc = transpile(self.qnn, backend)
+        
